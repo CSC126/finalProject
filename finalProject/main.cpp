5b2556a778eaa35aa35284b33a72078119d51f5d
Alan Mackiewicz <alanmackiewicz@yahoo.com>
2022-05-08 19:49:15 -0400

    Final Updates

--- finalProject/main.cpp
+++ finalProject/main.cpp
@@ -3,6 +3,8 @@
 #include <fstream>
 #include <string>
 using namespace std;
+
+//declaring all the functions
 char calculateLetterGrade(double);
 double findMinQuiz(double, double, double, double);
 double findAverageQuizScore(double, double, double, double);
@@ -11,13 +13,11 @@ double findAverageLabScore(double, double, double, double, double, double, doubl
 double findAverageTestScore(double, double);
 
 string firstName, lastName;
-float quiz1, quiz2, quiz3, quiz4;
-float lab1, lab2, lab3, lab4, lab5, lab6, lab7, lab8;
-float hw1, hw2, hw3, hw4, hw5, hw6, hw7;
-float exam1, exam2;
-float Thefinal;
+float quiz1, quiz2, quiz3, quiz4, lab1, lab2, lab3, lab4, lab5, lab6, lab7, lab8, hw1, hw2, hw3, hw4, hw5, hw6, hw7, exam1, exam2, Thefinal;
 double FINAL_AVERAGE;
 char LETTER_GRADE;
+double studentsInClass = 0, averageOfCourseAverage = 0;
+
 int main(){
     
     ifstream fin;
@@ -26,8 +26,9 @@ int main(){
     foutOne.open("gradesOut1.txt");
     foutTwo.open("gradesOut2.txt");
     
-    while(!fin.eof()){
-        fin >> firstName >> lastName >> quiz1 >> quiz2 >> quiz3 >> quiz4 >> lab1 >> lab2 >> lab3 >> lab4 >> lab5 >> lab6 >> lab7 >> lab8 >> hw1 >> hw2 >> hw3 >> hw4 >> hw5 >> hw6 >> hw7 >> exam1 >> exam2 >> Thefinal;
+    //The loop works by checking if all these variables can be defined in a row and if they can then the condition is met and the loop runs
+    while(fin >> firstName >> lastName >> quiz1 >> quiz2 >> quiz3 >> quiz4 >> lab1 >> lab2 >> lab3 >> lab4 >> lab5 >> lab6 >> lab7 >> lab8 >> hw1 >> hw2 >> hw3 >> hw4 >> hw5 >> hw6 >> hw7 >> exam1 >> exam2 >> Thefinal){
+       //calling all the functions and setting their values to variables to be used later
         double calcQuizWeight = findAverageQuizScore(quiz1, quiz2, quiz3, quiz4)*.20;
         double calcHwWeight = findAverageHomeworkScore(hw1, hw2, hw3, hw4, hw5, hw6, hw7)*.10;
         double calcLabWeight = findAverageLabScore(lab1, lab2, lab3, lab4, lab5, lab6, lab7, lab8)*.10;
@@ -36,16 +37,30 @@ int main(){
         FINAL_AVERAGE = calcQuizWeight + calcHwWeight + calcLabWeight + calcTestWeight + calcFinalWeight;
         LETTER_GRADE = calculateLetterGrade(FINAL_AVERAGE);
         
+        //formatting the output files correctly
         foutOne << firstName << ", " << lastName << endl << "Quiz Average: " << calcQuizWeight << endl
         << "Average Of Exams: " << calcTestWeight << endl << "Average of labs: " << calcLabWeight << endl << "Average of Homework: " << calcHwWeight << endl << "The Final: " << calcFinalWeight << endl << "The course Average: " << FINAL_AVERAGE << endl << endl;
         
         foutTwo << firstName << ", " << lastName << endl << "Course Average: " << FINAL_AVERAGE << endl << "Letter Grade: " << LETTER_GRADE << endl << endl;
         
+        //adding to two variables that will be printed after the while loop
+        studentsInClass++;
+        averageOfCourseAverage += FINAL_AVERAGE;
+        
     }
+        
+    foutTwo << "Total Students in the class: " << studentsInClass << endl << "Average of course average: " << (averageOfCourseAverage / studentsInClass);
+    
+    //closing the files as we do not need to add to them anymore
+    fin.close();
+    foutOne.close();
+    foutTwo.close();
     
     return 0;
 }
 
+
+//all the functions to use in the code
 char calculateLetterGrade(double a){
     char grade;
     if(a >= 90){
