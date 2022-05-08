497fdec5448737244b027e97d2ae01b56223cd59
Alan Mackiewicz <alanmackiewicz@yahoo.com>
2022-05-08 15:49:00 -0400

    Setup

--- finalProject/main.cpp
+++ finalProject/main.cpp
@@ -1,14 +1,7 @@
-//
-//  main.cpp
-//  finalProject
-//
-//  Created by Alan Mackiewicz on 5/2/22.
-//
-
 #include <iostream>
+#include <iomanip>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main(){
+    
     return 0;
 }
